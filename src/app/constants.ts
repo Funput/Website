06a0e47c9438f701e1d6c0aeb6f9685312f563d8
pcodedynamics <pcodedynamics@gmail.com>
2026-06-20 23:03:01 +0700

    Update Facebook URL in constants and modify footer email display

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const FUNPUT_CONSTANTS = {
   GITHUB_URL: 'https://github.com/pcodedynamics/Funput',
-  FACEBOOK_URL: 'https://www.facebook.com/funput',
+  FACEBOOK_URL: 'https://www.facebook.com/pulsefu',
   CONTACT_EMAIL: 'pcodedynamics@gmail.com',
   RELEASES_URL: 'https://github.com/pcodedynamics/Funput/releases',
 };
