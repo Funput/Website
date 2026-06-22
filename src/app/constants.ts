c6d343ade975440edb9084321612e41ca025229c
pcodedynamics <pcodedynamics@gmail.com>
2026-06-22 22:34:39 +0700

    Update GitHub URLs in llms.txt and constants.ts to reflect new repository ownership

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 export const FUNPUT_CONSTANTS = {
-  GITHUB_URL: 'https://github.com/pcodedynamics/Funput',
+  GITHUB_URL: 'https://github.com/PulseFu/Funput',
   FACEBOOK_URL: 'https://www.facebook.com/pulsefu',
   CONTACT_EMAIL: 'pcodedynamics@gmail.com',
-  RELEASES_URL: 'https://github.com/pcodedynamics/Funput/releases',
+  RELEASES_URL: 'https://github.com/PulseFu/Funput/releases',
 };
