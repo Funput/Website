9cbee77ebbc8c0d69a3d0e23c6442e958f56555f
pcodedynamics <pcodedynamics@gmail.com>
2026-07-31 00:32:13 +0700

    feat: add Android closed testing registration link and documentation URL in constants, and update platforms.html to reflect testing status and enhance user engagement

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,9 +1,15 @@
 export const FUNPUT_CONSTANTS = {
   GITHUB_URL: 'https://github.com/Funput/Funput',
   DOCS_URL: 'https://docs.funput.app/',
+  ANDROID_DOCS_URL: 'https://docs.funput.app/docs/install/android',
   FACEBOOK_URL: 'https://www.facebook.com/FunputIME',
   CONTACT_EMAIL: 'hello@funput.app',
   RELEASES_URL: 'https://github.com/Funput/Funput/releases',
   IOS_APP_STORE_URL: 'https://apps.apple.com/vn/app/id6788829996',
-  ANDROID_PLAY_STORE_URL: 'https://play.google.com/store/apps/details?id=app.funput.funput',
+  ANDROID_PLAY_STORE_URL:
+    'https://play.google.com/store/apps/details?id=app.funput.funput',
+  /** Mailto for Android closed-testing signup (Vietnamese subject/body). */
+  ANDROID_TRIAL_MAILTO:
+    'mailto:hello@funput.app?subject=%C4%90%C4%83ng%20k%C3%BD%20d%C3%B9ng%20th%E1%BB%AD%20Funput%20Android&body=Xin%20ch%C3%A0o%20Funput%2C%0A%0AT%C3%B4i%20mu%E1%BB%91n%20tham%20gia%20ki%E1%BB%83m%20th%E1%BB%AD%20kh%C3%A9p%20k%C3%ADn%20Funput%20tr%C3%AAn%20Android.%0A%0AEmail%20Google%20Play%20(Gmail)%20c%E1%BB%A7a%20t%C3%B4i%3A%20%0A%0AC%E1%BA%A3m%20%C6%A1n!',
 };
+
